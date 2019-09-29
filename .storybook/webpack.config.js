98813b1e9afef5e7a7cde63a174649cdd1366fa4
Hiswe <hiswehalya@gmail.com>
2019-09-29 16:54:57 +0200

    STORYBOOK – add

---
+++ .storybook/webpack.config.js
@@ -0,0 +1,23 @@
+const path = require('path');
+
+const componentsPath = path.resolve(__dirname, `../src`);
+
+// Support Scss and Pug
+// https://storybook.js.org/docs/configurations/custom-webpack-config/#full-control-mode
+module.exports = async ({ config, mode }) => {
+    // // https://vue-loader.vuejs.org/guide/pre-processors.html#sass
+    // config.module.rules.push({
+    //     test: /\.scss$/,
+    //     loaders: [`style-loader`, `css-loader`, `sass-loader`],
+    //     include: componentsPath,
+    // });
+
+    // https://www.npmjs.com/package/storybook-addon-vue-info
+    config.module.rules.push({
+        test: /\.vue$/,
+        loader: `storybook-addon-vue-info/loader`,
+        enforce: `post`,
+    });
+
+    return config;
+};
